593a2b72104260df7390d3f8ae9d997a8bbf69b5
Water.Li <lizongbin@hotmail.com>
2022-07-10 12:58:14 +0800

    app 可以运行，react 版本全部固定在17.0.2

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,11 +1,12 @@
 import 'antd/dist/antd.less'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Layout from './Layout'
 import Test from './Test'
 
 const App = () => {
   return (
-    <Test />
+    <Layout />
   )
 }
 
