5dfa4f06c80bd53f9aabb928bf8d72e6baa2427b
Water.Li <lizongbin@hotmail.com>
2022-07-10 12:40:38 +0800

    studio能运行

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,12 +1,11 @@
 import 'antd/dist/antd.less'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Layout from './Layout'
 import Test from './Test'
 
 const App = () => {
   return (
-    <Layout />
+    <Test />
   )
 }
 
