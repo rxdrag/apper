7fa6d97c062942bbd81cbc570a3f412d7c63de31
Water.Li <lizongbin@hotmail.com>
2022-07-15 10:39:44 +0800

    开始深入学习webpack，最简单例子可编译

--- plugins/first/webpack.config.js
+++ plugins/first/webpack.config.js
@@ -1,12 +1,37 @@
+const path = require('path');
+
 const config = {
-  entry: 'index.js',
+  entry: './src/index.ts',
+  mode:"development",
   output: {
     filename: '[name].js',
-    path: __dirname + '/dist'
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   },
   module: {
     rules: [
-      { test: /\.txt$/, use: 'raw-loader' }
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html?$/,
+        loader: require.resolve('file-loader'),
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+
     ]
   }
 };
