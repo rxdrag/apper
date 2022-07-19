92283a21300a77149ac522e2930c8fc18c2df5c9
Water.Li <lizongbin@hotmail.com>
2022-07-20 07:57:08 +0800

    backup

--- packages/studio/src/App.tsx
+++ packages/studio/src/App.tsx
@@ -1,8 +1,50 @@
-import React from "react";
-import AppEditor from "./AppEditor";
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React from 'react';
+const { Header, Content, Footer } = Layout;
 
-export default function App(){
-  return (
-    <AppEditor />
-  )
-}
+const App = () => (
+  <Layout className="rx-studio">
+    <Header className="header">
+      <div className='logo'>
+        <svg style={{ width: "40px", height: "40px" }} viewBox="0 0 24 24">
+          <defs>
+            <linearGradient id="logo_color" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="0%" stopColor="#3ca9f2" />
+              <stop offset="90%" stopColor="#3a29e6" />
+              <stop offset="100%" stopColor="#3ca9f2" />
+            </linearGradient>
+          </defs>
+          <path
+            style={{ fill: "url(#logo_color)" }}
+            d="M23 11.5L19.95 10.37C19.69 9.22 19.04 8.56 19.04 8.56C17.4 6.92 14.75 6.92 13.11 8.56L11.63 10.04L5 3C4 7 5 11 7.45 14.22L2 19.5C2 19.5 10.89 21.5 16.07 17.45C18.83 15.29 19.45 14.03 19.84 12.7L23 11.5M17.71 11.72C17.32 12.11 16.68 12.11 16.29 11.72C15.9 11.33 15.9 10.7 16.29 10.31C16.68 9.92 17.32 9.92 17.71 10.31C18.1 10.7 18.1 11.33 17.71 11.72Z"
+          />
+        </svg>
+        Apper
+      </div>
+      <Menu
+        theme="light"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        items={new Array(15).fill(null).map((_, index) => {
+          const key = index + 1;
+          return {
+            key,
+            label: `nav ${key}`,
+          };
+        })}
+      />
+    </Header>
+    <Content className='content'>
+
+    </Content>
+    <Footer
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      Apper ©2022 Created by 悠闲的水
+    </Footer>
+  </Layout>
+);
+
+export default App
