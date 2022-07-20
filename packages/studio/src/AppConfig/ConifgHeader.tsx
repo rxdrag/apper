42d3b209e9f40d7437d2800c50572657f33b068b
Water.Li <lizongbin@hotmail.com>
2022-07-20 18:35:54 +0800

    backup

--- packages/studio/src/AppConfig/ConifgHeader.tsx
+++ packages/studio/src/AppConfig/ConifgHeader.tsx
@@ -1,10 +1,54 @@
+import { AppstoreOutlined, QuestionCircleOutlined, GithubOutlined, HomeOutlined } from "@ant-design/icons"
+import { Button, Divider, Menu, MenuProps, Space, Tabs } from "antd"
 import { Header } from "antd/lib/layout/layout"
-import React from "react"
+import React, { useCallback } from "react"
 import { memo } from "react"
+import AvatarMenu from "../AppManager/AvatarMenu"
+
 
 const ConifgHeader = memo(() => {
+  const onChange = useCallback((key: string) => {
+    console.log(key);
+  }, []);
+
   return (
-    <Header className="header"></Header>
+    <Header className="header">
+      <HomeOutlined />
+      <Divider type='vertical' />
+      <div>CRM管理</div>
+      <Menu
+        className="app-config-menu"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        items={[
+          {
+            key:1,
+            label: "基础配置",
+          },
+          {
+            key:2,
+            label: "应用设计",
+          },
+          {
+            key:3,
+            label: "权限管理",
+          },
+        ]}
+      />
+
+      <Space>
+        <Button className='min-button' size='large' shape="circle" icon={<QuestionCircleOutlined />} />
+        <Button
+          className='min-button'
+          size='large'
+          shape="circle"
+          icon={<GithubOutlined />}
+          href="https://github.com/rxdrag/apper"
+          target="_blank"
+        />
+        <AvatarMenu />
+      </Space>
+    </Header >
   )
 })
 
