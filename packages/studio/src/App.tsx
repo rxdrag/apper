c2e5fed6d45447d9768e1c1ae42a149989d23147
Water.Li <lizongbin@hotmail.com>
2022-07-20 08:29:04 +0800

    backup

--- packages/studio/src/App.tsx
+++ packages/studio/src/App.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, Layout, Menu } from 'antd';
+import { Breadcrumb, Divider, Layout, Menu } from 'antd';
 import React from 'react';
 const { Header, Content, Footer } = Layout;
 
@@ -21,18 +21,7 @@ const App = () => (
         </svg>
         Apper
       </div>
-      <Menu
-        theme="light"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        items={new Array(15).fill(null).map((_, index) => {
-          const key = index + 1;
-          return {
-            key,
-            label: `nav ${key}`,
-          };
-        })}
-      />
+      <Divider className='logo-divider' type='vertical' />
     </Header>
     <Content className='content'>
 
