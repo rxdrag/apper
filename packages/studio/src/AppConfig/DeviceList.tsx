2a68b3b2c71db0f41e36219d5b8f531d8f0d8eae
Water.Li <lizongbin@hotmail.com>
2022-07-20 20:52:47 +0800

    Update DeviceList.tsx

--- packages/studio/src/AppConfig/DeviceList.tsx
+++ packages/studio/src/AppConfig/DeviceList.tsx
@@ -1,3 +1,6 @@
+import { SettingOutlined, EditOutlined, EllipsisOutlined } from "@ant-design/icons"
+import { Row, Col, Card, Button } from "antd"
+import Meta from "antd/lib/card/Meta"
 import React from "react"
 import { memo } from "react"
 
@@ -6,7 +9,62 @@ const DeviceList = memo(() => {
     <div className="content-show-block">
       <div className="config-content">
         <h2>应用设计</h2>
-        <a href="/design-app/xxx">设计Admin</a>
+        <Row className="app-row" gutter={24}>
+          <Col span={6}>
+            <Card
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <Button key="design" shape="round" type="primary" href="/design-app/xxx">去设计</Button>,
+                <Button key="preview" shape="round" >去预览</Button>,
+              ]}
+            >
+              <Meta
+                title="PC设计"
+              />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <Button key="design" shape="round" type="primary" href="/design-app/xxx">去设计</Button>,
+                <Button key="preview" shape="round" >去预览</Button>,
+              ]}
+            >
+              <Meta
+                title="H5设计"
+              />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card
+              cover={
+                <img
+                  alt="example"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                />
+              }
+              actions={[
+                <Button key="design" shape="round" type="primary" href="/design-app/xxx">去设计</Button>,
+                <Button key="preview" shape="round" >去预览</Button>,
+              ]}
+            >
+              <Meta
+                title="管理端设计"
+              />
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
   )
