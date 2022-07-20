05ba2783b2b6e7064e944c6e3ef6d5c965a807b2
Water.Li <lizongbin@hotmail.com>
2022-07-20 11:18:34 +0800

    backup

--- packages/studio/src/widgets/AppManagebar.tsx
+++ packages/studio/src/widgets/AppManagebar.tsx
@@ -1,15 +1,23 @@
-import { ImportOutlined, PlusOutlined } from "@ant-design/icons";
+import { ImportOutlined, InfoCircleOutlined, PlusOutlined, SearchOutlined } from "@ant-design/icons";
 import { Button, Input, Space } from "antd";
 import React from "react";
 
-const AppManagebar = ()=>{
+const { Search } = Input;
+
+const AppManagebar = () => {
   return (
     <div className="app-manage-bar">
+      <Input
+        className="search"
+        placeholder="请输入应用名称或关键字"
+        suffix={
+          <SearchOutlined className="search-icon" />
+        }
+      />
       <Space>
+        <Button icon={<ImportOutlined />}>导入应用</Button>
         <Button type="primary" icon={<PlusOutlined />}>创建应用</Button>
-        <Button type="primary" icon={<ImportOutlined />}>导入应用</Button>
       </Space>
-      <Input allowClear size="middle"className="search" />
     </div>
   )
 }
